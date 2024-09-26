f2ad654920d3b2ae1090525cf3cbb3ab26fcd905
C1yde11B <Clydellb0@gmail.com>
2024-09-26 09:16:22 -0500

    Initial Commit

---
+++ MyFavBooks/MyFavBooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFavBooksApp.swift
+//  MyFavBooks
+//
+//  Created by AM Student on 9/26/24.
+//
+
+import SwiftUI
+
+@main
+struct MyFavBooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
