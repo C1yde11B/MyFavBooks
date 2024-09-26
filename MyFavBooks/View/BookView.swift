d3216cf3659dc34d6aec3571dbef6d05070fae76
C1yde11B <Clydellb0@gmail.com>
2024-09-26 10:45:23 -0500

    A

---
+++ MyFavBooks/View/BookView.swift
@@ -0,0 +1,18 @@
+//
+//  BookView.swift
+//  MyFavBooks
+//
+//  Created by AM Student on 9/26/24.
+//
+
+import SwiftUI
+
+struct BookView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BookView()
+}
